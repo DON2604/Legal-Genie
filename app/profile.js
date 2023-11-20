30bfe255252cc2e9b2d6e4b4a0300e07f19139e8
Mrinmay <mrinmaydas2604@gmail.com>
2023-11-20 14:32:49 +0530

    not yet responsive

--- app/profile.js
+++ app/profile.js
@@ -1,65 +1,139 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "expo-image";
+import { FontSize, FontFamily, Color } from "../GlobalStyles";
 
-const ProfilePage = () => {
+const Profile = () => {
   return (
-    <View style={styles.container}>
+    <View style={styles.profile}>
+      <LinearGradient
+        style={styles.bg}
+        locations={[0, 1]}
+        colors={["#2e2e2e", "#181818"]}
+      />
       <Image
         style={styles.profileImage}
-        source={{
-          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjVuQI6QXrY3HiEurXkZzbQ5Eikj4c30-Cuw&usqp=CAU",
-        }}
+        contentFit="cover"
+        source={require("../assets/profile-image.png")}
       />
-      <View style={styles.detailsContainer}>
-        <Text style={styles.username}>Cheems Balltze</Text>
-        <Text style={styles.bio}>A brief bio about yourself goes here.</Text>
-        <Text style={styles.additionalText}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        deserunt sequi ipsum temporibus velit reprehenderit in aliquam culpa
-        molestiae, corrupti nostrum. Laboriosam, nemo. Aut exercitationem nisi
-        commodi possimus! Deleniti, quo.
-      </Text>
+      <View style={styles.usernameParent}>
+        <View style={[styles.username, styles.nameLayout]}>
+          <View style={[styles.username, styles.nameLayout]}>
+            <Text style={[styles.username2, styles.genvTypo]}>Username</Text>
+            <Text style={[styles.genv, styles.genvTypo]}>GenV</Text>
+            <View style={[styles.usernameChild, styles.profile1Position]} />
+          </View>
+        </View>
+        <View style={[styles.firstName, styles.nameLayout]}>
+          <View style={[styles.username, styles.nameLayout]}>
+            <Text style={[styles.username2, styles.genvTypo]}>First Name</Text>
+            <Text style={[styles.genv, styles.genvTypo]}>Bhupendra</Text>
+            <View style={[styles.usernameChild, styles.profile1Position]} />
+          </View>
+        </View>
+        <View style={[styles.lastName, styles.nameLayout]}>
+          <View style={[styles.username, styles.nameLayout]}>
+            <Text style={[styles.username2, styles.genvTypo]}>Last name</Text>
+            <Text style={[styles.genv, styles.genvTypo]}>Jogi</Text>
+            <View style={[styles.usernameChild, styles.profile1Position]} />
+          </View>
+        </View>
+
       </View>
-      
+      <Text style={[styles.profile1, styles.profile1Position]}>Profile</Text>
     </View>
+
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column", // Change to column to stack components vertically
-    alignItems: "center",
-    padding: wp("4%"), // 4% of the screen width
-    flex: 1, // Make the container take the whole vertical space
+  nameLayout: {
+    height: 56,
+    width: 315,
+    left: 0,
+    position: "absolute",
+  },
+  genvTypo: {
+    fontSize: FontSize.size_sm,
+    textAlign: "left",
+    left: 0,
+    position: "absolute",
+  },
+  profile1Position: {
+    left: "50%",
+    position: "absolute",
+  },
+  bg: {
+    width: '100%',
+    backgroundColor: "transparent",
+    left: 0,
+    top: 0,
+    position: "absolute",
+    height: '100%',
   },
   profileImage: {
-    width: wp("20%"), // 20% of the screen width
-    height: wp("20%"), // 20% of the screen width
-    borderRadius: wp("10%"), // to make it circular
-    marginBottom: hp("2%"), // Space between image and details
+    top: 169,
+    right: 137,
+    borderRadius: 36,
+    width: 100,
+    height: 100,
+    position: "absolute",
   },
-  detailsContainer: {
-    flex: 1,
-    alignItems: "center", // Center details horizontally
+  username2: {
+    fontWeight: "500",
+    fontFamily: FontFamily.robotoMedium,
+    color: Color.colorWhite,
+    textAlign: "left",
+    top: 0,
+  },
+  genv: {
+    top: 32,
+    fontFamily: FontFamily.robotoRegular,
+    color: Color.colorGray,
+    textAlign: "left",
+    color: Color.colorWhite,
+  },
+  usernameChild: {
+    marginLeft: -158,
+    top: 56,
+    borderStyle: "solid",
+    borderColor: Color.colorWhite,
+    borderTopWidth: 1,
+    width: 316,
+    height: 1,
   },
   username: {
-    fontSize: wp("5%"), // Adjust font size based on screen width
-    fontWeight: "bold",
-    marginBottom: hp("1%"), // 1% of the screen height
+    top: 0,
   },
-  bio: {
-    fontSize: wp("3.5%"), // Adjust font size based on screen width
+  firstName: {
+    top: 80,
+  },
+  lastName: {
+    top: 160,
+  },
+  usernameParent: {
+    top: 338,
+    left: 30,
+    height: 216,
+    width: 315,
+    position: "relative",
+  },
+  profile1: {
+    marginLeft: -47.5,
+    top: 62,
+    fontSize: 32,
+    fontWeight: "600",
+    fontFamily: FontFamily.robotoBold,
     textAlign: "left",
   },
-  additionalText: {
-    fontSize: wp("4%"), // Adjust font size based on screen width
-    color: "gray",
-    textAlign: "center",
+  profile: {
+    backgroundColor: Color.colorWhite,
+    flex: 1,
+    width: "100%",
+    overflow: "hidden",
+    height: 812,
   },
 });
 
-export default ProfilePage;
+export default Profile;
